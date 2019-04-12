306df05397a46260929984e764e92ff9ebf569cd
Kent Larsen <kent@h-larsen.dk>
2019-04-12 20:11:28 +0200

    Created Modify-methods in CelestialObjectController.

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -43,6 +44,48 @@ namespace StarChart.Controllers
             if (!co.Any()) return NotFound();
             co.ForEach(e => e.Satellites = _context.CelestialObjects.Where(o => o.Id == e.Id).ToList());
             return Ok(co);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]CelestialObject co)
+        {
+            _context.CelestialObjects.Add(co);
+            _context.SaveChanges();
+            return CreatedAtRoute("GetById", new { id = co.Id }, co);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject co)
+        {
+            var existing = _context.CelestialObjects.Find(id);
+            if (existing == null) return NotFound();
+            existing.Name = co.Name;
+            existing.OrbitalPeriod = co.OrbitalPeriod;
+            existing.OrbitedObjectId = co.OrbitedObjectId;
+            _context.Update(existing);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            var existing = _context.CelestialObjects.Find(id);
+            if (existing == null) return NotFound();
+            existing.Name = name;
+            _context.Update(existing);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existing = _context.CelestialObjects.Find(id);
+            if (existing == null) return NotFound();
+            _context.Remove(existing);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
