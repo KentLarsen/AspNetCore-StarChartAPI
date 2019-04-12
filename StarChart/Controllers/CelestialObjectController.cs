7a5235ca73922272a14a8e32b1866fbd58254063
Kent Larsen <kent@h-larsen.dk>
2019-04-12 19:49:27 +0200

    Create Get-Methods

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,32 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var co = _context.CelestialObjects.Find(id);
+            if (co == null) return NotFound();
+            co.Satellites = _context.CelestialObjects.Where(o => o.OrbitedObjectId == id).ToList();
+            return Ok(co);
+        }
+
+        [HttpGet("{name:string}")]
+        public IActionResult GetByName(string name)
+        {
+            var co = _context.CelestialObjects.Where(o => o.Name == name).ToList();
+            if (co == null) return NotFound();
+            co.ForEach(e => e.Satellites = _context.CelestialObjects.Where(o => o.Id == e.Id).ToList());
+            return Ok(co);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var co = _context.CelestialObjects.ToList();
+            if (co == null) return NotFound();
+            co.ForEach(e => e.Satellites = _context.CelestialObjects.Where(o => o.Id == e.Id).ToList());
+            return Ok(co);
+        }
     }
 }
